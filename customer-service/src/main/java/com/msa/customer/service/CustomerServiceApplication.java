98e26ff310864d84842f9625264288a2732a4a2c
Nelson Tello N <tellonelson@hotmail.com>
2024-09-27 22:07:49 -0500

    update commit

--- customer-service/src/main/java/com/msa/customer/service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/msa/customer/service/CustomerServiceApplication.java
@@ -2,9 +2,11 @@ package com.msa.customer.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
---
+++ service-registry/src/test/java/com/msa/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
